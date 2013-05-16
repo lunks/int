257695e0be27c0ac31a68b888351f5e195ecfb85
Pedro Nascimento <pnascimento@gmail.com>
2013-05-16 15:49:41 -0300

    Requiring formula and using master for now.

--- int.rb
+++ int.rb
@@ -1,4 +1,5 @@
+require 'formula'
 class Int < Formula
-  url 'git@github.com:Helabs/int.git', :using => :git, :branch => 'stable'
+  url 'git@github.com:Helabs/int.git', :using => :git, :branch => 'master'
   version '0.0.1'
 end
