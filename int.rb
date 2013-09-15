5493cbfe5ac8af71d6c40c4c5ac66a00c837d32a
Pedro Nascimento <pnascimento@gmail.com>
2013-09-15 14:58:13 -0300

    Release 0.0.1

--- int.rb
+++ int.rb
@@ -1,5 +1,8 @@
 require 'formula'
 class Int < Formula
+  homepage 'https://github.com/Helabs/int'
+  url 'https://github.com/Helabs/int/archive/0.0.1.tar.gz'
+  sha1 'a5ec57ea15f4c8733a6d72f76b7dd6a57fadc196'
   head 'https://github.com/Helabs/int.git', :using => :git
 
   def install
