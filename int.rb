962e911141a6858fb16a75ce1b7cf798e77610cd
Pedro Nascimento <pnascimento@gmail.com>
2013-09-15 15:10:53 -0300

    Updating sha1.

--- int.rb
+++ int.rb
@@ -2,7 +2,7 @@ require 'formula'
 class Int < Formula
   homepage 'https://github.com/Helabs/int'
   url 'https://github.com/Helabs/int/archive/0.0.1.tar.gz'
-  sha1 'a5ec57ea15f4c8733a6d72f76b7dd6a57fadc196'
+  sha1 'a4a47f5c4d1a46badd4c75186434a344db5f8611'
   head 'https://github.com/Helabs/int.git', :using => :git
 
   def install
